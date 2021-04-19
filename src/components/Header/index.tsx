ad2abd1f3605851fb0298cbfc8d4b6bae20d0251
Diego Fernandes <diego.schell.f@gmail.com>
2021-04-19 17:02:42 -0300

    Aula 02

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import styles from './styles.module.scss';
+
+export function Header() {
+  const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+    locale: ptBR,
+  });
+
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="Podcastr" />
+
+      <p>O melhor para você ouvir, sempre</p>
+
+      <span>{currentDate}</span>
+    </header>
+  );
+}
